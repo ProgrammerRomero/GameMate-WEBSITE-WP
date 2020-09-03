08d1e309103888c41614e0e1b9941c680082522e
programmer <0xprogrammerx0@gmail.com>
2020-09-03 19:49:04 +1000

    First Template created

--- app/public/wp-content/themes/GameMate/page.php
+++ app/public/wp-content/themes/GameMate/page.php
@@ -2,22 +2,29 @@
 
 <?php get_header(); ?>
 
-<?php
-    while( have_posts() ) : the_post();
-?>
-    <h1><?php the_title(); ?></h1>
+    <main class="contenedor pagina seccion con-sidebar">
+        <div class="contenido-principal">
+            <?php
+            while( have_posts() ) : the_post();
+            ?>
+            <h1 class="text-center texto-primario"><?php the_title(); ?></h1>
 
-    <!-- Calling featured image -->
-    <?php if(has_post_thumbnail() ) :
-        the_post_thumbnail('portrait');
-        endif;
-    ?>
+                <!-- Calling featured image -->
+                <?php if(has_post_thumbnail() ) :
+                    the_post_thumbnail('portrait', array('class' => 'imagen-destacada'));
+                    endif;
+                ?>
 
-    <?php the_content(); ?>
-    Writen By  <?php the_author(); ?>
-    Created <?php the_date(); ?>
+            <?php the_content(); ?>
+            Writen By  <?php the_author(); ?>
+            Created <?php the_date(); ?>
 
-<?php
-    endwhile;
-?>
+            <?php
+                endwhile;
+            ?>
+
+        </div>
+        <?php get_sidebar(); ?>
+    </main>
+
 <?php get_footer(); ?>
---
+++ app/public/wp-content/themes/GameMate/sidebar.php
@@ -0,0 +1,4 @@
+
+<aside class="sidebar">
+            <h1 class="texto-primario text-center">sidebar here</h1>
+</aside>
